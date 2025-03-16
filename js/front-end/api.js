b7afe0c16b4366c2b8710a706e3d8906666282dd
Enrico Malta <90053556+enricomalta@users.noreply.github.com>
2025-03-16 10:26:41 -0300

    Update api.js

--- js/front-end/api.js
+++ js/front-end/api.js
@@ -1,5 +1,5 @@
 // const API_URL = "https://ton-flower-land-back-end.vercel.app"; // URL do seu back-end
-const API_URL = "http://192.168.0.100:3000"
+const API_URL = "https://ton-flower-land-back-end.vercel.app/"
 let isProcessing = false; // Variável de controle para evitar múltiplos cliques rápidos
 export let items = []; // Inicializando o array de itens
 
