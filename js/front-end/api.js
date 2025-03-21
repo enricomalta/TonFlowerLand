dd2897e2fb816a5f93d022ca2df4a13aa063abc5
Enrico Malta <90053556+enricomalta@users.noreply.github.com>
2025-03-21 18:47:57 -0300

    Update api.js

--- js/front-end/api.js
+++ js/front-end/api.js
@@ -1,6 +1,6 @@
 import { playOpen } from './index.js';
-// const API_URL = "https://ton-flower-land-back-end.vercel.app"; // URL do seu back-end
-const API_URL = "http://192.168.0.100:3000"
+const API_URL = "https://ton-flower-land-back-end.vercel.app"; // URL do seu back-end
+// const API_URL = "http://192.168.0.100:3000"
 let isProcessing = false; // Variável de controle para evitar múltiplos cliques rápidos
 export let items = []; // Inicializando o array de itens
 
